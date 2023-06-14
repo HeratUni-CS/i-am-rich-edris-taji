43220eb4398b374cb563577c26e3b2ab2519a29f
Edria Taji <116660805+edris-taji@users.noreply.github.com>
2023-06-14 09:50:36 +0430

    last changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-home: Scaffold(
-  backgroundColor: Colors.black26,
-  appBar: AppBar(
-    title: Text('I Am Rich ! '),
-    backgroundColor: Colors.indigo,
-  ),
-  body: Center(
-    child: Image(
-      image: AssetImage('images/daimond.jpg'),
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.black26,
+      appBar: AppBar(
+        title: Text('I Am Rich ! '),
+        backgroundColor: Colors.indigo,
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/daimond.jpg'),
+        ),
+      ),
+      // test
     ),
-  ),
-),
-    )
-  );
+  ));
 }
